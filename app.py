b86da3601a8c9c046885a517eb612a895b655f75
MyEyesAreCode <107769265+MyEyesAreCode@users.noreply.github.com>
2022-06-18 19:15:27 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ---- HEADER SECTION ----
+st.subheader("WELCOME TO")
+st.title("LUKE'S STARTER PY WEBPAGE!!")
+st.write("Learning how to code using VS Code, This is a webpage to help you learn faster, Click the Learn more link")
+st.write("[Learn More >](https://www.python.org/downloads/release/python-3105/)")
